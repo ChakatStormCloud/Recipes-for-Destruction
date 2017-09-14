1ef9d57703d442f87ed19acbefc1c12142af89c9
StormCloud <RabidRenamon@gmail.com>
2017-09-13 21:10:02 -0600

    added some recipes

--- src/main/java/StormCloud/recipesfordestruction/explosion/api/Vanilla.java
+++ src/main/java/StormCloud/recipesfordestruction/explosion/api/Vanilla.java
@@ -11,17 +11,22 @@ public class Vanilla {
 		System.out.println("Trying to add recipes");
 		Item planks = Item.getItemFromBlock(Block.getBlockFromName("planks")); 
 		Item stick = Item.getByNameOrId("stick");
+		Item cobble = Item.getItemFromBlock(Block.getBlockFromName("cobblestone"));
+		
 		
 		ExplosionRecipeHandler.addExplosionResult(Item.getItemFromBlock(Block.getBlockFromName("chest")), 6f, planks,planks,planks,planks,planks,planks,planks,planks);
 		ExplosionRecipeHandler.addExplosionResult(Item.getItemFromBlock(Block.getBlockFromName("crafting_table")), 4f, planks,planks,planks,planks);
-		
+		ExplosionRecipeHandler.addExplosionResult(Item.getItemFromBlock(Block.getBlockFromName("furnace")), 9f, cobble,cobble,cobble,cobble,cobble,cobble,cobble,cobble);
 		ExplosionRecipeHandler.addExplosionResult(planks, 2f, stick,stick);
-		ExplosionRecipeHandler.addExplosionResult(Item.getItemFromBlock(Block.getBlockFromName("cobble")), 16, Item.getItemFromBlock(Block.getBlockFromName("gravel")));
+		ExplosionRecipeHandler.addExplosionResult(cobble, 14f, Item.getItemFromBlock(Block.getBlockFromName("gravel")));
+		
 		ExplosionRecipeHandler.handleBlockExplosion(Block.getBlockFromName("dirt"),RecipeType.DROP);
 		ExplosionRecipeHandler.handleBlockExplosion(Block.getBlockFromName("grass"),RecipeType.DROP);
 		ExplosionRecipeHandler.handleBlockExplosion(Block.getBlockFromName("stone"),RecipeType.BREAK);
 		ExplosionRecipeHandler.handleBlockExplosion(Block.getBlockFromName("chest"),RecipeType.BREAK);
 		ExplosionRecipeHandler.handleBlockExplosion(Block.getBlockFromName("crafting_table"),RecipeType.BREAK);
+		ExplosionRecipeHandler.handleBlockExplosion(Block.getBlockFromName("furnace"), RecipeType.BREAK);
+		ExplosionRecipeHandler.handleBlockExplosion(Block.getBlockFromName("cobblestone"), RecipeType.BREAK);
 		
 	}
 	
