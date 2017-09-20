2cebfd851a1c38edab445ca92fc74d57a7f7ee9a
StormCloud <RabidRenamon@gmail.com>
2017-09-20 11:43:28 -0600

    Added Recipes

--- src/main/java/StormCloud/recipesfordestruction/explosion/api/Vanilla.java
+++ src/main/java/StormCloud/recipesfordestruction/explosion/api/Vanilla.java
@@ -23,10 +23,11 @@ public class Vanilla {
 		addDebris(Blocks.CHEST, 6f, planks,planks,planks,planks,planks,planks,planks,planks);
 		addDebris(Blocks.CRAFTING_TABLE, 4f, planks,planks,planks,planks);
 		addDebris(Blocks.FURNACE, 9f, cobble,cobble,cobble,cobble,cobble,cobble,cobble,cobble);
-		addDebris(Blocks.PLANKS, 2f, new ItemStack(Items.STICK,2));
+		addDebris(Blocks.PLANKS, 3f, new ItemStack(Items.STICK,2));
 		addDebris(Blocks.COBBLESTONE, 14f, new ItemStack(Item.getItemFromBlock(Blocks.GRAVEL)));
 		addDebris(Blocks.BED,4f,planks,planks,planks,wool,wool,wool);
 		addDebris(Blocks.LEAVES,0.5f,stick);
+		addDebris(Blocks.LEAVES2,0.5f,stick);
 		addDebris(Blocks.DISPENSER,8f,cobble,cobble,cobble,cobble,cobble,cobble,cobble,redstone,new ItemStack(Items.BOW, 1));
 		addDebris(Items.BOW,2f,new ItemStack(Items.STICK,3),new ItemStack(Items.STRING,3));
 		addDebris(Blocks.DROPPER,8f,cobble,cobble,cobble,cobble,cobble,cobble,cobble,redstone);
@@ -37,18 +38,76 @@ public class Vanilla {
 		addDebris(Blocks.NOTEBLOCK,6f,planks,planks,planks,planks,planks,planks,planks,planks,redstone);
 		addDebris(Blocks.BOOKSHELF,4f,planks,planks,planks,planks,planks,planks,new ItemStack(Items.BOOK, 3));
 		addDebris(Items.BOOK,2f,new ItemStack(Items.PAPER,3));
-		addDebris(Blocks.OAK_FENCE,5f,planks,stick);
-		addDebris(Blocks.SPRUCE_FENCE,5f,new ItemStack(Blocks.PLANKS,1,1),stick);
-		addDebris(Blocks.BIRCH_FENCE,5f,new ItemStack(Blocks.PLANKS,1,2),stick);
-		addDebris(Blocks.JUNGLE_FENCE,5f,new ItemStack(Blocks.PLANKS,1,3),stick);
-		addDebris(Blocks.ACACIA_FENCE,5f,new ItemStack(Blocks.PLANKS,1,4),stick);
-		addDebris(Blocks.DARK_OAK_FENCE,5f,new ItemStack(Blocks.PLANKS,1,5),stick);
+		
+		addDebris(Blocks.OAK_DOOR,4f, new ItemStack(Blocks.PLANKS,2,0));
+		addDebris(Blocks.OAK_FENCE,4f, new ItemStack(Blocks.PLANKS,1,0),stick);
+		addDebris(Blocks.OAK_FENCE_GATE,6f, new ItemStack(Blocks.PLANKS,2,0),new ItemStack(Items.STICK,4));
+		addDebris(Blocks.OAK_STAIRS,3f, new ItemStack(Blocks.PLANKS,1,0));
+		
+		addDebris(Blocks.SPRUCE_DOOR,4f, new ItemStack(Blocks.PLANKS,2,1));
+		addDebris(Blocks.SPRUCE_FENCE,4f, new ItemStack(Blocks.PLANKS,1,1),stick);
+		addDebris(Blocks.SPRUCE_FENCE_GATE,6f, new ItemStack(Blocks.PLANKS,2,1),new ItemStack(Items.STICK,4));
+		addDebris(Blocks.SPRUCE_STAIRS,3f, new ItemStack(Blocks.PLANKS,1,1));
+		
+		addDebris(Blocks.BIRCH_DOOR,4f, new ItemStack(Blocks.PLANKS,2,2));
+		addDebris(Blocks.BIRCH_FENCE,4f, new ItemStack(Blocks.PLANKS,1,2),stick);
+		addDebris(Blocks.BIRCH_FENCE_GATE,6f, new ItemStack(Blocks.PLANKS,2,2),new ItemStack(Items.STICK,4));
+		addDebris(Blocks.BIRCH_STAIRS,3f, new ItemStack(Blocks.PLANKS,1,2));
+		
+		addDebris(Blocks.JUNGLE_DOOR,4f, new ItemStack(Blocks.PLANKS,2,3));
+		addDebris(Blocks.JUNGLE_FENCE,4f, new ItemStack(Blocks.PLANKS,1,3),stick);
+		addDebris(Blocks.JUNGLE_FENCE_GATE,6f, new ItemStack(Blocks.PLANKS,2,3),new ItemStack(Items.STICK,4));
+		addDebris(Blocks.JUNGLE_STAIRS,3f, new ItemStack(Blocks.PLANKS,1,3));
+		
+		addDebris(Blocks.ACACIA_DOOR,4f, new ItemStack(Blocks.PLANKS,2,4));
+		addDebris(Blocks.ACACIA_FENCE,4f, new ItemStack(Blocks.PLANKS,1,4),stick);
+		addDebris(Blocks.ACACIA_FENCE_GATE,6f, new ItemStack(Blocks.PLANKS,2,4),new ItemStack(Items.STICK,4));
+		addDebris(Blocks.ACACIA_STAIRS,3f, new ItemStack(Blocks.PLANKS,1,4));
+		
+		addDebris(Blocks.DARK_OAK_DOOR,4f, new ItemStack(Blocks.PLANKS,2,5));
+		addDebris(Blocks.DARK_OAK_FENCE,4f, new ItemStack(Blocks.PLANKS,1,5),stick);
+		addDebris(Blocks.DARK_OAK_FENCE_GATE,6f, new ItemStack(Blocks.PLANKS,2,5),new ItemStack(Items.STICK,4));
+		addDebris(Blocks.DARK_OAK_STAIRS,3f, new ItemStack(Blocks.PLANKS,1,5));
+		
+		addMetaDebris(Item.getItemFromBlock(Blocks.STONEBRICK),8f,new int[][]{{0,3},{1},{2}},
+				new ItemStack[][]{{new ItemStack(Blocks.STONEBRICK,1,2)},{new ItemStack(Blocks.MOSSY_COBBLESTONE,1)},{new ItemStack(Blocks.COBBLESTONE,1)}});
 		addMetaDebris(Item.getItemFromBlock(Blocks.LOG),6f,new int[][]{{0,4,8,12},{1,5,9,13},{2,6,10,14},{3,7,11,15}},
 				new ItemStack[][]{{new ItemStack(Blocks.PLANKS,4,0)},{new ItemStack(Blocks.PLANKS,4,1)},{new ItemStack(Blocks.PLANKS,4,2)},{new ItemStack(Blocks.PLANKS,4,3)}});
 		addMetaDebris(Item.getItemFromBlock(Blocks.LOG2),6f,new int[][]{{0,4,8,12},{1,5,9,13}},
 				new ItemStack[][]{{new ItemStack(Blocks.PLANKS,4,4)},{new ItemStack(Blocks.PLANKS,4,5)}});
 		
+		
+		addRecipe(Blocks.OAK_DOOR,RecipeType.BREAK);
 		addRecipe(Blocks.OAK_FENCE,RecipeType.BREAK);
+		addRecipe(Blocks.OAK_FENCE_GATE,RecipeType.BREAK);
+		addRecipe(Blocks.OAK_STAIRS,RecipeType.BREAK);
+		
+		addRecipe(Blocks.SPRUCE_DOOR,RecipeType.BREAK);
+		addRecipe(Blocks.SPRUCE_FENCE,RecipeType.BREAK);
+		addRecipe(Blocks.SPRUCE_FENCE_GATE,RecipeType.BREAK);
+		addRecipe(Blocks.SPRUCE_STAIRS,RecipeType.BREAK);
+		
+		addRecipe(Blocks.BIRCH_DOOR,RecipeType.BREAK);
+		addRecipe(Blocks.BIRCH_FENCE,RecipeType.BREAK);
+		addRecipe(Blocks.BIRCH_FENCE_GATE,RecipeType.BREAK);
+		addRecipe(Blocks.BIRCH_STAIRS,RecipeType.BREAK);
+		
+		addRecipe(Blocks.JUNGLE_DOOR,RecipeType.BREAK);
+		addRecipe(Blocks.JUNGLE_FENCE,RecipeType.BREAK);
+		addRecipe(Blocks.JUNGLE_FENCE_GATE,RecipeType.BREAK);
+		addRecipe(Blocks.JUNGLE_STAIRS,RecipeType.BREAK);
+		
+		addRecipe(Blocks.ACACIA_DOOR,RecipeType.BREAK);
+		addRecipe(Blocks.ACACIA_FENCE,RecipeType.BREAK);
+		addRecipe(Blocks.ACACIA_FENCE_GATE,RecipeType.BREAK);
+		addRecipe(Blocks.ACACIA_STAIRS,RecipeType.BREAK);
+		
+		addRecipe(Blocks.DARK_OAK_DOOR,RecipeType.BREAK);
+		addRecipe(Blocks.DARK_OAK_FENCE,RecipeType.BREAK);
+		addRecipe(Blocks.DARK_OAK_FENCE_GATE,RecipeType.BREAK);
+		addRecipe(Blocks.DARK_OAK_STAIRS,RecipeType.BREAK);
+				
+				
 		addRecipe(Blocks.BOOKSHELF,RecipeType.BREAK);
 		addRecipe(Blocks.NOTEBLOCK,RecipeType.BREAK);
 		addRecipe(Blocks.CAULDRON,RecipeType.BREAK);
@@ -70,9 +129,10 @@ public class Vanilla {
 		addRecipe(Blocks.LOG2,RecipeType.BREAK);
 		addRecipe(Blocks.BED,RecipeType.BREAK);
 		addRecipe(Blocks.LEAVES,RecipeType.BREAK);
+		addRecipe(Blocks.LEAVES2,RecipeType.BREAK);
 		addRecipe(Blocks.DROPPER,RecipeType.BREAK);
 		addRecipe(Blocks.DISPENSER,RecipeType.BREAK);
-		
+		addRecipe(Blocks.STONEBRICK,RecipeType.BREAK);
 		
 	}
 	//maybe 
