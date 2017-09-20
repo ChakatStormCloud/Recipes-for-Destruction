555c23484886a7ea38edc0e8c277e656ed0be1da
StormCloud <RabidRenamon@gmail.com>
2017-09-19 19:29:44 -0600

    version update

--- src/main/java/StormCloud/recipesfordestruction/MainLib.java
+++ src/main/java/StormCloud/recipesfordestruction/MainLib.java
@@ -4,7 +4,7 @@ public class MainLib {
 	
 	public static final String MODID = "recipesfordestruction";
 	public static final String MODNAME = "Recipes for Destruction";
-	public static final String VERSION = "0.3.1";
+	public static final String VERSION = "0.3.2";
 	public static final String CLIENTCLASS = "StormCloud.recipesfordestruction.proxy.ClientProxy";
 	public static final String SERVERCLASS = "StormCloud.recipesfordestruction.proxy.ServerProxy";
 	//public static final String GUIFACTORYCLASS = "com.stormcloud.PoweredArmour.Client.Gui.GuiFactory";
